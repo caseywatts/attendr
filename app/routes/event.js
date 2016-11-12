e941990db2b5da6f62669ad859d98d7de649482c
Casey Watts <casey.s.watts@gmail.com>
2016-11-12 11:28:23 -0500

    use ember-cli-mirage and ember-data for events

--- app/routes/event.js
+++ app/routes/event.js
@@ -2,11 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    let events = [
-      Ember.Object.create({ id: 1, name: 'alx code & coffee', location: 'ALX', time: new Date() }),
-      Ember.Object.create({ id: 2, name: 'arlington code & coffee', location: 'ARL', time: new Date() }),
-      Ember.Object.create({ id: 3, name: 'escape room', location: 'ALX', time: new Date() })
-    ];
-    return events.findBy('id', Number(params.eventId));
+    return this.get('store').find('event', params.eventId);
   }
 });
---
+++ mirage/fixtures/events.js
@@ -0,0 +1,5 @@
+export default [
+  { id: 1, name: 'alx code & coffee', location: 'ALX', time: new Date() },
+  { id: 2, name: 'arlington code & coffee', location: 'ARL', time: new Date() },
+  { id: 3, name: 'escape room', location: 'ALX', time: new Date() }
+];
