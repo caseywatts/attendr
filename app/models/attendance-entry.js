a1f198ff0b65be50e06daa86351ebf84fc357254
Casey Watts <casey.s.watts@gmail.com>
2016-11-13 10:59:47 -0500

    display present vs absent lists

--- app/models/attendance-entry.js
+++ app/models/attendance-entry.js
@@ -1,8 +1,13 @@
 import DS from 'ember-data';
+const { computed } = Ember;
+const { equal } = computed;
 
 export default DS.Model.extend({
   user: DS.belongsTo('user'),
   event: DS.belongsTo('event'),
   time: DS.attr('date'),
-  state: DS.attr('string')
+  state: DS.attr('string'),
+
+  isPresent: equal('state', 'present'),
+  isAbsent: equal('state', 'absent')
 });
--- app/models/event.js
+++ app/models/event.js
@@ -1,8 +1,13 @@
 import DS from 'ember-data';
+const { computed } = Ember;
+const { filterBy } = computed;
 
 export default DS.Model.extend({
   attendanceEntries: DS.hasMany('attendance-entry'),
   name: DS.attr('string'),
   location: DS.attr('string'),
-  time: DS.attr('date')
+  time: DS.attr('date'),
+
+  presentEntries: filterBy('attendanceEntries', 'isPresent'),
+  absentEntries: filterBy('attendanceEntries', 'isAbsent')
 });
